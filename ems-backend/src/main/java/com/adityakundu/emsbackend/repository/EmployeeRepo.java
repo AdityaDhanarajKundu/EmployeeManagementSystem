b94b6658e9ef80a9a341f2ed642b67f6474c7be1
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-02-26 10:17:24 +0530

    employee jpa repo

---
+++ ems-backend/src/main/java/com/adityakundu/emsbackend/repository/EmployeeRepo.java
@@ -0,0 +1,10 @@
+package com.adityakundu.emsbackend.repository;
+
+import com.adityakundu.emsbackend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+
+}
