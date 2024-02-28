1a8bda153512297862e9cbc01b41a070d2c9b67c
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-02-28 22:52:59 +0530

    file for the client side api

---
+++ fullstack/ems-frontend/src/services/EmployeeService.js
@@ -0,0 +1,8 @@
+// file for the client side api for the http rest api request handling
+import axios from 'axios';
+
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/employees";
+
+export function getEmployees() {
+    return axios.get(EMPLOYEE_API_BASE_URL);
+}
