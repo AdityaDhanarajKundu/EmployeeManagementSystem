73487d280e384632836ce4176351c0b3c7c20ca0
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-02-29 00:47:07 +0530

    connected the backend java springboot server to fetch the employee data

--- fullstack/ems-frontend/src/components/ListEmployees.jsx
+++ fullstack/ems-frontend/src/components/ListEmployees.jsx
@@ -1,31 +1,48 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { getEmployees } from '../services/EmployeeService';
 
 function ListEmployees() {
-  const sampleData = [
-    {
-        "id": 1,
-        "firstName": "John",
-        "lastName": "Stone",
-        "email": "johnstone@gmail.com",
-    },
-    {
-        "id": 2,
-        "firstName": "Smiti",
-        "lastName": "Sharma",
-        "email": "smitisharma@gmail.com",
-    },
-    {
-        "id": 3,
-        "firstName": "Rahul",
-        "lastName": "Gupta",
-        "email": "rahulgupta@gmail.com",
-    },
-  ];
+  const [employees, setEmployees] = useState([]);  
+
+  useEffect(()=>{
+    getEmployees().then((response)=>{
+        setEmployees(response.data);
+    }).catch((error)=>{
+        console.error(error);
+    });
+  },[employees]);
+    
+  // static data to create the table for the employees
+//   const sampleData = [
+//     {
+//         "id": 1,
+//         "firstName": "John",
+//         "lastName": "Stone",
+//         "email": "johnstone@gmail.com",
+//     },
+//     {
+//         "id": 2,
+//         "firstName": "Smiti",
+//         "lastName": "Sharma",
+//         "email": "smitisharma@gmail.com",
+//     },
+//     {
+//         "id": 3,
+//         "firstName": "Rahul",
+//         "lastName": "Gupta",
+//         "email": "rahulgupta@gmail.com",
+//     },
+//   ];
     
   return (
-    <div className='container'>
-      <h1 className='text-center' style={{marginBottom: "20px", marginTop: "15px"}}>List of Employees</h1>
-      <table className='table table-striped table-bordered'>
+    <div className="container">
+      <h1
+        className="text-center"
+        style={{ marginBottom: "20px", marginTop: "15px" }}
+      >
+        List of Employees
+      </h1>
+      <table className="table table-striped table-bordered">
         <thead>
           <tr>
             <th>Employee Id</th>
@@ -35,14 +52,14 @@ function ListEmployees() {
           </tr>
         </thead>
         <tbody>
-            {sampleData.map((employee)=>(
-                <tr key={employee.id}>
-                    <td>{employee.id}</td>
-                    <td>{employee.firstName}</td>
-                    <td>{employee.lastName}</td>
-                    <td>{employee.email}</td>
-                </tr>
-            ))}
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.fname}</td>
+              <td>{employee.lname}</td>
+              <td>{employee.email}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
