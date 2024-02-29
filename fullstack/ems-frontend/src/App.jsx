aefc1d1c093b17a526f88a5b0dcd3811d68ac367
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-02-29 10:42:06 +0530

    routes

--- fullstack/ems-frontend/src/App.jsx
+++ fullstack/ems-frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ListEmployees from "./components/ListEmployees";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
-      <ListEmployees />
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ListEmployees />} />
+          <Route path="/employees" element={<ListEmployees />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
