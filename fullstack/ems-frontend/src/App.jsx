4b9fe0c5fbb62e2983eaa12d1d4c421488f2ff59
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-02-29 09:47:38 +0530

    footer component

--- fullstack/ems-frontend/src/App.jsx
+++ fullstack/ems-frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import Header from "./components/Header"
-import ListEmployees from "./components/ListEmployees"
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ListEmployees from "./components/ListEmployees";
 
 function App() {
-
   return (
-    <>  
+    <>
       <Header />
       <ListEmployees />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ fullstack/ems-frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+        <span style={{marginTop: "30px"}}>Made By Aditya Dhanaraj Kundu</span>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
