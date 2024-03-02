ac6343d4f560c5b45a9f59bdc75fe078bcdc1e62
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-03-02 19:32:56 +0530

    base url changed to aws server url

--- fullstack/ems-frontend/src/services/EmployeeService.js
+++ fullstack/ems-frontend/src/services/EmployeeService.js
@@ -1,7 +1,8 @@
 // file for the client side api for the http rest api request handling
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/employees";
+const EMPLOYEE_API_BASE_URL =
+  "http://springboot-mysql-ems-env.eba-cqssvdhy.ap-southeast-2.elasticbeanstalk.com/api/employees";
 
 export function getEmployees() {
     return axios.get(EMPLOYEE_API_BASE_URL);
